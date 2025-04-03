68b02f5e3f03aaa9e8e42e7ef0ec2b16318bb922
CactusHead100 <cactushead100@gmail.com>
2025-04-03 13:12:01 +1300

    Created some classes and synced Git

---
+++ src/Accounts.java
@@ -0,0 +1,12 @@
+public class Accounts{
+    public enum ACCOUNTS{Everyday, Savings, Current};
+    private final String CUSTOMERNAME;
+    private final int ACCOUNTNUMBER;
+    private final String CUSTOMERADDRESS;
+    
+
+    Accounts(){
+        CUSTOMERNAME = "tree";
+    }
+
+}
